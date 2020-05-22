91b0ece9085eb2e82c441e5fa6d6ab8db5dd14b0
j0sh1x <aljoshua.hell@gmail.com>
2020-05-22 12:04:35 +0000

    losify

---
+++ lineage_tank.mk
@@ -0,0 +1,21 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit device configuration for Fire TV Stick 2
+$(call inherit-product, device/amazon/tank/full_tank.mk)
+
+# Screen Resolution
+TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_HEIGHT := 1024
+
+# Device identifier
+PRODUCT_DEVICE := tank
+PRODUCT_NAME := lineage_tank
+PRODUCT_BRAND := google
+PRODUCT_MODEL := tank
+PRODUCT_MANUFACTURER := amzn
+
+# Override the fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=SHARP/an_np40/an_np40:5.1/LMY47M/20180306:user/release-keys \
+    PRIVATE_BUILD_DESC="an_np40-user 5.1 LMY47M 20180306 release-keys"
